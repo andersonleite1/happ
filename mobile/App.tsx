a2476be73f56e809f544475362478f3be06774f1
Anderson Leite <usuario2020.dev@gmail.com>
2020-11-13 17:37:06 -0300

    Add pop-up in map marker

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, {Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, {Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons'
 
 import mapMarker from './src/images/map-marker.png';
 
@@ -20,12 +21,25 @@ export default function App() {
       > 
         <Marker
           icon={mapMarker}
+          calloutAnchor={{
+            x: 3.1,
+            y: 0.8,
+          }}
           coordinate={{
             latitude: -14.8521574,
             longitude: -40.8475082,
           }}
-        />
+        >
+          <Callout tooltip onPress={() => {alert('Olá')}}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>
+                Salão Novo Visual
+              </Text>
+            </View>
+          </Callout>
+        </Marker>
       </MapView>
+      
     </View>
   );
 }
@@ -39,4 +53,18 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 15,
+  },
+  
 });
