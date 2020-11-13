1301f6f428b3bac5dba8159afb534521d939df16
Anderson Leite <usuario2020.dev@gmail.com>
2020-11-13 18:15:18 -0300

    Add font Nunito

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,11 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import MapView, {Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
-import { Feather } from '@expo/vector-icons'
+import { Feather } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
 import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold, 
+    Nunito_700Bold, 
+    Nunito_800ExtraBold,
+  });
+  
+  if(!fontsLoaded){
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <MapView 
@@ -69,6 +81,7 @@ const styles = StyleSheet.create({
   },
   calloutText: {
     color: '#0089a5',
+    fontFamily: 'Nunito_700Bold',
     fontSize: 15,
   },
 
@@ -90,7 +103,8 @@ const styles = StyleSheet.create({
     elevation: 3,
   },
   footerText: {
-    color: '#8fa7b3'
+    fontFamily: 'Nunito_700Bold',
+    color: '#8fa7b3',
   },
 
   createSalonButton: {
